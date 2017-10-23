edd89ba5a04ddf086c779548b243059b7ffbf916
lion <153375424@qq.com>
2017-10-23 11:59:23 +0800

    commit

---
+++ swagger/src/main/java/com/learn/swagger/IndexController.java
@@ -0,0 +1,15 @@
+package com.learn.swagger;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class IndexController {
+
+	@RequestMapping("/")
+	public String index(HttpServletRequest request){
+		return "index controller";
+	}
+}
